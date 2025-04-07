eb2929745ae300d302a5ce1c47d1487371c2ee7c
EmilyBartman <90109327+EmilyBartman@users.noreply.github.com>
2025-04-07 19:17:16 -0400

    Update app_tcm.py

--- TCM/app_tcm.py
+++ TCM/app_tcm.py
@@ -393,8 +393,12 @@ elif page == "Tongue Health Check":
             db.collection("tongue_scans").document(submission_id).set(result)
 
             st.subheader(translate("🧪 Analysis Results", target_lang))
-           st.info(f"""🔍 **{translate('Detected TCM Pattern:', target_lang)}** {prediction_TCM}  
-            🧠 **{translate('Western View:', target_lang)}** {prediction_Western}""")
+            st.markdown(f"""
+            **🔍 {translate('Detected TCM Pattern:', target_lang)}**: {prediction_TCM}  
+            **🧠 {translate('Western View:', target_lang)}**: {prediction_Western}
+            """)
+
+
            
             
             # Extract RGB values from the avg_color_str
